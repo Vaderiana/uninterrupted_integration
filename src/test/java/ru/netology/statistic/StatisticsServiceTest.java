0c2e1c6f5ad43ed95bee1fc1de11b75050ae1fac
Vaderiana <120415111+Vaderiana@users.noreply.github.com>
2023-02-17 16:28:12 +0300

    add COVEREDRATIO

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -21,8 +21,8 @@ public class StatisticsServiceTest {
     void findMax2() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = { 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
